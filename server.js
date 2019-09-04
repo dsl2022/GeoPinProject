89f839bb2eecdc2747caec998f9096aefd10ad7f
tmm <blocklocmedia@gmail.com>
2019-09-04 10:38:44 -0400

    added mongoDB pin and user models

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// remove deprecationWarning
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose
+	.connect(process.env.MONGO_URI, { userNewUriPaser: true })
+	.then(() => {
+		console.log('DB connected');
+	})
+	.catch((err) => console.error(err));
 
 const server = new ApolloServer({
 	typeDefs,
